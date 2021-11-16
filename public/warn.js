ddf82634fc6f2c43345ab2d44843f5479364afd3
Eugene Kiprop Sang <85967099+eugenesang@users.noreply.github.com>
2021-11-16 12:34:48 +0300

    Create warn.js

---
+++ public/warn.js
@@ -0,0 +1,2 @@
+console.log("%c Warning!", "color: red; font-size: 1.5cm; ");
+console.log("%c This Is a developer tool, if a friend showed some code to write here, it might destroy or make your account vulnerable to computer errors", "color:#0067ef; font-size:0.5cm;")
